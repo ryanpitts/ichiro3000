16a345b526eec05833be81b9f0d1e433d9cd2b69
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-04-06 20:29:03 -0700

    initialize ichiro3000

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ichiro3000
+
+A little Python script that helps you track a hitter's progress toward an MLB milestone. This is totally just roughed in, but it's working.
+
+## Setup
+Make your virtualenv and install the requirements. Note: This uses redis for storage, so you'll need to start up redis as a background service.
+
+Edit CONFIG in `fetch.py` to:
+* identify a player by MLB ID and team abbreviation
+* set `start_count` and an optional `target_count` for the milestone
+* identify which hitting events should increment the player's `current_count`
+
+## TODO
+* Refactor away some ugliness
+* Hook `handle_match()` up to a Twitter bot
+* Deploy to Heroku and stick it on a cron
---
+++ fetch.py
@@ -0,0 +1,185 @@
+import argparse, os, sys, traceback
+import hashlib
+import redis
+import requests
+from datetime import date
+from pyquery import PyQuery as PQ
+
+DATA_ROOT = 'http://gd2.mlb.com/components/game/mlb/'
+
+# ICHIRO MLB ID 400085
+CONFIG = {
+    'playerID': '621043', #borrowing Carlos Correa's 4/6/2016 for testing
+    'team': 'HOU', #borrowing Carlos Correa's 4/6/2016 for testing
+    'player_name': 'Ichiro',
+    'start_count': 2935,
+    'target_count': 3000,
+    'events_tracked': ['Single', 'Double', 'Triple', 'Home Run'],
+}
+
+
+def create_redis_connection():
+    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+    r = redis.from_url(redis_url)
+    
+    return r
+
+
+def flush_redis():
+    '''
+    Empty the current redis db
+    '''
+    r = create_redis_connection()
+    r.flushdb()
+    
+    print 'Redis keys set to: ' + str(r.keys())
+    
+    
+def check_redis():
+    '''
+    Check keys in current redis db
+    '''
+    r = create_redis_connection()
+    
+    for key in r.keys():
+        print '{0}: {1}'.format(key, r.get(key))
+
+
+def clean_the_lines():
+    flush_redis()
+    check_redis()
+    
+    print 'The lines are cleaned.'
+
+
+def init_counts(r):
+    '''
+    Make sure redis has values for start, current, and target counts,
+    and return those counts for possible calculations. 
+    '''
+    start = r.get('start_count')
+    if not start:
+        start = CONFIG['start_count']
+        r.set('start_count', start)
+        print 'Start count initialized.'
+        
+    current = r.get('current_count')
+    if not current:
+        current = CONFIG['start_count']
+        r.set('current_count', current)
+
+    if 'target_count' in CONFIG:
+        target = r.get('target_count')
+        if not target:
+            target = CONFIG['target_count']
+            r.set('target_count', target)
+            print 'Target count initialized.'
+    else:
+        target = 0
+        
+    return int(start), int(current), int(target)
+
+
+def fetch_events():
+    r = create_redis_connection()
+    # borrowing Correa 4/6/2016 for testing
+    # delete this key to force a match each scrape
+    # and test addition to `current_count`
+    r.delete('gid_2016_04_06_houmlb_nyamlb_1-621043-AB4')
+    
+    # some constants for this scrape
+    playerID = CONFIG['playerID']
+    team = CONFIG['team']
+    today = date.today()
+    
+    # scrape the MLB game list page
+    game_day_url = DATA_ROOT + 'year_{0}/month_{1}/day_{2}/'.format(today.year, '{:02d}'.format(today.month), '{:02d}'.format(today.day))
+    page = PQ(game_day_url)
+    # find the links on page
+    game_links = [PQ(link).attr('href') for link in page('li a')]
+    # we only care about game data links for player's team
+    game_links = [link.strip('/') for link in game_links if 'gid' in link and team.lower() in link]
+
+    # iterate through team's games for the day
+    for gameID in game_links:
+        # get the player's batter data file for this game
+        data_url = game_day_url + gameID
+        data_url += "/batters/{0}.xml".format(playerID)
+        page = PQ(data_url)
+        # just the at-bat events please
+        atbats = page('atbats ab')
+
+        # iterate through player's at-bats
+        for index, event in enumerate(atbats):
+            atbat = index+1
+            # see if we've seen this at-bat
+            rkey = "{0}-{1}-AB{2}".format(gameID, playerID, atbat)
+            stored = r.get(rkey)
+
+            # store results of new at-bats so we only
+            # match against events we haven't seen
+            if not stored:
+                result = PQ(event).attr("event")
+                match = result in CONFIG['events_tracked']
+                r.set(rkey, result)
+                
+                # if we match, do a thing
+                if match:
+                    handle_match(result)
+               
+    print 'Done with scrape.' 
+
+
+def handle_match(result):
+    r = create_redis_connection()
+    # get our counts
+    start, current, target = init_counts(r)
+    # update current total
+    new_total = current+1
+    r.set('current_count', new_total)
+    
+    # make a nice message for this match
+    player_name = CONFIG['player_name']
+    message = "{0} {1}!".format(player_name, result.lower())
+    
+    if target > 0:
+        message += " Only {:,} to go till {:,}.".format((target-current), target)
+    else:
+        message += " That's {:,} so far.".format(current)
+
+    print(message)
+    
+
+def process_args(arglist=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--check', action='store_const', const=True)
+    parser.add_argument('--flush', action='store_const', const=True)
+    parser.add_argument('--clean', action='store_const', const=True)
+    args = parser.parse_args()
+    
+    return args
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = process_args(args)
+        
+    if args.flush:
+        flush_redis()
+    elif args.check:
+        check_redis()
+    elif args.clean:
+        clean_the_lines()
+    else:
+        fetch_events()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception, e:
+        sys.stderr.write('\n')
+        traceback.print_exc(file=sys.stderr)
+        sys.stderr.write('\n')
+        sys.exit(1)
+
