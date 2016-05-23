d0ded0b0f47cd79baaf7946f887a662a9501de2d
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-05-22 17:35:09 -0700

    update base count

--- fetch.py
+++ fetch.py
@@ -24,7 +24,7 @@ CONFIG = {
     'playerID': '400085',
     'team': 'MIA',
     'player_name': 'Ichiro',
-    'start_count': 2940,
+    'start_count': 2956,
     'target_count': 3000,
     'events_tracked': ['Single', 'Double', 'Triple', 'Home Run'],
 }
